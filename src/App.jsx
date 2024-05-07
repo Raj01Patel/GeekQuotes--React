9753ee7d4cccb51f8851724d1b69493a2efb38f2
Raj Patel <patelraj200208@gmail.com>
2024-05-07 17:22:24 +0530

    GeekQuotes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Quotes from "./Components/Quotes/Quotes"
+import Header from "./Components/Header/Header"
+import Footer from "./Components/Footer/Footer"
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Quotes />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
